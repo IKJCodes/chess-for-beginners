8923ead86e5b5634a732d729710c69d03a38b1e8
IKJCodes <solangensayers@hotmail.com>
2026-05-14 18:21:56 -0500

    Update and rename piece-names.jsx to piece-classification.jsx

--- piece-classification.jsx
+++ piece-classification.jsx
@@ -1,9 +1,9 @@
 {/** ChessName function */}
-function ChessName(ChessName_props) {
+function Chessclassification_props(Classification_props) {
     return <div>
         {/** Makes it so in the App.jsx I can rename it to for example, a rook or a bishop */}
-        name={ChessName_props.name}
+        {Classification_props.name}
     </div>
 }
 
-export default ChessName
+export default Chessclassification_props
